cbe90ca879a6f341904e879038e04eb5da855497
karim-hammami <hammamik790@gmail.com>
2023-01-27 11:44:05 +0100

    new scripts for management, alacritty theme to carbonfox

---
+++ dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+yay -S neovim alacritty zsh
---
+++ symlink.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+ln -sf ~/.dotfiles/i3/ ~/.config/
+ln -sf ~/.dotfiles/nvim/ ~/.config
+ln -sf ~/.dotfiles/alacritty/ ~/.config/ 
+ln -sf ~/.dotfiles/wallpaper/ ~/ 
+ln -sf ~/.dotfiles/.zshrc ~/ 
+
